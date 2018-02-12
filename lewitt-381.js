97c90ae1e39fdc0b4f413c41b5f55d4ccc226370
Jie(Jackie) Ji <35789241+jijie21@users.noreply.github.com>
2018-02-12 05:37:21 -0800

    Create lewitt-381.js

---
+++ lewitt-381.js
@@ -0,0 +1,17 @@
+
+fill(150,150,150);//grey
+rect(0,0,width/2,height/2);
+noStroke();
+
+fill(255,215,0);//yellow
+rect(width/2,0,width/2,height/2);  
+noStroke();
+
+
+fill(135,206,235);//blue
+rect(width/2,height/2,width/2,height/2);
+noStroke();
+
+fill(250,128,114);//red
+rect(0,height/2,width/2,height/2);
+noStroke();
